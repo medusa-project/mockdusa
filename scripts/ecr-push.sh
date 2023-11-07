8c5d8ea9b49611962bb3b0b88d70e3cd745f76fb
Alex Dolski <alexd@n-ary.net>
2023-11-07 12:12:19 -0600

    Use docker buildx to build for both amd64 and arm64

--- scripts/ecr-push.sh
+++ scripts/ecr-push.sh
@@ -5,7 +5,8 @@
 
 source scripts/env.sh env.list
 
-docker build -t mockdusa .
 eval $(aws ecr get-login --no-include-email --region $AWS_REGION --profile $AWS_PROFILE)
+# N.B.: linux/amd64 is the only platform supported by GitHub Actions currently
+docker buildx build --platform linux/amd64,linux/arm64 --push -t mockdusa .
 docker tag $DOCKER_TAG $ECR_HOST/$DOCKER_TAG
 docker push $ECR_HOST/$DOCKER_TAG
